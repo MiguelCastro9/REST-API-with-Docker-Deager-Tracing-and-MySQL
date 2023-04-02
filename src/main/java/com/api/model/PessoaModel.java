8c8c2bbb2b625b3e88652a70e2d0931c5a227690
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-04-02 14:10:25 -0300

    criando crud para teste deager tracing

---
+++ src/main/java/com/api/model/PessoaModel.java
@@ -0,0 +1,62 @@
+package com.api.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Miguel Castro
+ */
+@Entity
+@Table(name = "pessoas")
+public class PessoaModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "nome", nullable = false)
+    private String nome;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public PessoaModel() {
+    }
+
+    public PessoaModel(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
